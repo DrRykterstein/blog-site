539b33724e54a7b15f386a5ba25c633c483cb5fa
Rio <riostockton@gmail.com>
2021-03-29 19:01:46 +0100

    Fixed configuration for production mode

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://tech-blog.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://tech-blog-five.vercel.app';

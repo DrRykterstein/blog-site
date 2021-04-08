690bcc7a88fe4b4a4eba3a73cb1ffba6f9acd917
Rio <riostockton@gmail.com>
2021-04-08 19:45:00 +0100

    Changed config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : `http://${process.env.VERCEL_URL}`;
+export const server = dev ? 'http://localhost:3000' : `https://technology-blog-wecxvwfsh-terioch.vercel.app`;

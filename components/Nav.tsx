40462c3e842b52faba02fa53e995e50c2f8ff133
Rio <riostockton@gmail.com>
2021-04-27 14:51:44 +0100

    Minor refactors

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,46 +5,48 @@ import Components from "../components/Components";
 import navStyles from "../styles/Nav.module.scss";
 
 const Nav: React.FC = () => {
-  const { nav, title, login, formContainer } = navStyles;
-  const { Login } = Components;
+	const { nav, title, login, formContainer } = navStyles;
+	const { Login } = Components;
 
-  // Toggle state to display or close form
-  const [displayForm, setDisplayForm] = useState(false);
+	// Toggle state to display or close form
+	const [displayForm, setDisplayForm] = useState(false);
 
-  // Get displayForm state from session storage
-  useEffect(() => {
-    displayForm && sessionStorage.getItem(JSON.parse('display-form-data'));
-    setDisplayForm(displayForm);
-  }, []);
+	// Get displayForm state from session storage
+	useEffect(() => {
+		displayForm && sessionStorage.getItem(JSON.parse("display-form-data"));
+		setDisplayForm(displayForm);
+	}, []);
 
-  // Store displayForm state within session storage
-  useEffect(() => {
-    sessionStorage.setItem('display-form-data', JSON.stringify(displayForm));
-  });
+	// Store displayForm state within session storage
+	useEffect(() => {
+		sessionStorage.setItem(
+			"display-form-data",
+			JSON.stringify(displayForm)
+		);
+	});
 
-  return (
-    <React.Fragment>
-      <Toolbar className={nav}>
-        <Link href="/">
-          <h1 className={title}>Technology Blog</h1>
-        </Link>
-        <Button 
-          variant="contained"
-          color="primary"
-          size="large"
-          onClick={() => setDisplayForm(true)}
-        >
-          <p className={login}>Demo Sign In</p>
-        </Button>
-      </Toolbar>
-        {
-          displayForm && 
-          <div className={formContainer}>
-            <Login formDisplay={{ displayForm, setDisplayForm }} />
-          </div>
-        }
-    </React.Fragment>
-  );
-}
+	return (
+		<React.Fragment>
+			<Toolbar className={nav}>
+				<Link href="/">
+					<h1 className={title}>Tech Blog</h1>
+				</Link>
+				<Button
+					variant="contained"
+					color="primary"
+					size="large"
+					onClick={() => setDisplayForm(true)}
+				>
+					<p className={login}>Sign In</p>
+				</Button>
+			</Toolbar>
+			{displayForm && (
+				<div className={formContainer}>
+					<Login formDisplay={{ displayForm, setDisplayForm }} />
+				</div>
+			)}
+		</React.Fragment>
+	);
+};
 
 export default Nav;
